eafff62b97e5956a24f6f6daf70ccf32e6f6bdd4
Skrop55 <yarinhaham21@gmail.com>
2023-01-16 17:39:42 +0200

    first commit

---
+++ Clicker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public void ClickTheButton() {
+        GameMannager.moneyCount += 1;
+    }
+}
---
+++ GameMannager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMannager : MonoBehaviour
+{
+    public static int moneyCount;
+    public GameObject Money;
+    public int IternalMoney;
+
+    void Start()
+    {
+        moneyCount = 0;
+    }
+
+    void Update()
+    {
+        IternalMoney = moneyCount; 
+        Money.GetComponent<Text>().text = "Money: " + moneyCount;
+    }
+}
